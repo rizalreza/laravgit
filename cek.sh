03a081e5eaf2db6f89ee057e469794626682fe3a
rizalreza <rizalrezamaulana.rrm@gmail.com>
2018-01-17 16:34:23 +0700

    Edit cek.sh tok

--- cek.sh
+++ cek.sh
@@ -3,7 +3,7 @@
 for d in *; do
 	if [[ -d $d/.git ]]
 	then
-	    echo $d "directory git ditemukan"
+	    echo $d "directory git ditemukan AAAA"
 	else
 	    echo $d "directory git tidak ditemukan"
 	fi
